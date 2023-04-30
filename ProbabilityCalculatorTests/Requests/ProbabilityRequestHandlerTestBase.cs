71017a7e9bf39957da86db9c94a4e84b1c2f45fb
patrick howell <patrick.howell@outlook.com>
2023-04-30 14:38:45 +0100

    refactor: resolved nullable warnings for test base class

--- ProbabilityCalculatorTests/Requests/ProbabilityRequestHandlerTestBase.cs
+++ ProbabilityCalculatorTests/Requests/ProbabilityRequestHandlerTestBase.cs
@@ -5,16 +5,17 @@ namespace ProbabilityCalculatorTests.Requests
 {
     public class ProbabilityRequestHandlerTestBase<T>
     {
-        protected Mock<ILogger<T>> _mockLogger;
+        protected Mock<ILogger<T>>? _mockLogger;
 
         protected void VerifyLoggerCall(LogLevel expectededLevel, string expectedMessage)
         {
+            Assert.NotNull(_mockLogger);
             _mockLogger.Verify(mock =>
                 mock.Log(It.Is<LogLevel>(level => level == expectededLevel),
                     It.IsAny<EventId>(),
                     It.Is<It.IsAnyType>((v, t) => v.ToString() == expectedMessage),
                     It.IsAny<Exception>(),
-                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), Times.Once);
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Once);
         }
     }
 }
