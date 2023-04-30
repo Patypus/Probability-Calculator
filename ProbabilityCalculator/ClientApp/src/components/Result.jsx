138e032aa798c0240cd683ee150a443013a7fcf3
patrick howell <patrick.howell@outlook.com>
2023-04-30 20:06:48 +0100

    style: spacing for result component

--- ProbabilityCalculator/ClientApp/src/components/Result.jsx
+++ ProbabilityCalculator/ClientApp/src/components/Result.jsx
@@ -3,7 +3,7 @@ import { Loading } from './Loading'
 
 export function Result ({ value, loading }) {
     return (
-        <div className="mb-3">
+        <div className="mt-3">
             <h3>Result:</h3>
             <div>
             {loading 
